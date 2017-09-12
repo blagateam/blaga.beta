4f981560c45e65b12ed4e2769c9fc124d9262f70
Imbuzan Radu <imbuzanradu@yahoo.com>
2017-09-12 19:17:42 +0300

    Fix archive style

--- src/pages/home/home.page.js
+++ src/pages/home/home.page.js
@@ -6,6 +6,7 @@ import { Header } from '../../components/header/header.component';
 import { ProfileComponent } from '../../components/profile/profile.component';
 import { TimelineComponent } from '../../components/timeline/timeline.component';
 import { ChatSidebar } from '../../components/chat-sidebar/chat-sidebar.component';
+import { ArchiveComponent } from '../../components/archive/archive.component'
 
 export class HomePage extends Component {
     render() {
@@ -14,7 +15,7 @@ export class HomePage extends Component {
                 <Header />
                 <div className="content">
                     <ProfileComponent user={this.props.user} />
-                    <TimelineComponent />
+                    <ArchiveComponent />
                     <ChatSidebar />
                 </div>
             </div>
