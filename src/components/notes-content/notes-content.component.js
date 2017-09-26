e1da12b1c5230c00a96c9a8b4f57e0f73e5abe6d
Imbuzan Radu <imbuzanradu@yahoo.com>
2017-09-26 15:03:12 +0300

    make the add notes button work

--- src/components/notes-content/notes-content.component.js
+++ src/components/notes-content/notes-content.component.js
@@ -4,13 +4,8 @@ import './notes-content.style.scss';
 
 export class NotesContent extends Component {
 
-    constructor(...args)
-    {
+    constructor(...args) {
         super(...args);
-
-        this.state={
-            notes: []
-        }
     }
 
     render() {
@@ -18,18 +13,17 @@ export class NotesContent extends Component {
             <div className="notes-content-component">
                 <div className="TopContent">
                     <div className="NotesTitle">
-                    <h3>{this.props.title}</h3>
+                        <h3>{this.props.title}</h3>
                     </div>
                     <div className="TopButtons">
-                    <button className="MaximizeButton">&#10066;</button>
-                    <button className="CloseButton">&#10006;</button>
+                        <button className="MaximizeButton">&#10066;</button>
+                        <button className="CloseButton">&#10006;</button>
                     </div>
                 </div>
                 <div className="NotesText">
-                <p>{this.props.noteText}</p>
+                    <p>{this.props.noteText}</p>
                 </div>
             </div>
-
-            )
+        )
     }
 }
--- src/components/timeline/timeline.component.js
+++ src/components/timeline/timeline.component.js
@@ -1,5 +1,5 @@
 // Import framework specific functions
-import {Component, h} from 'preact';
+import { Component, h } from 'preact';
 import './timeline.style.scss';
 
 import { NotesContent } from '../notes-content/notes-content.component';
@@ -22,18 +22,27 @@ export class TimelineComponent extends Component {
     }
 
     adaugaNotita() {
-        let database = firebase.database();
-        let note = this.refs.note.value;
-        let user = firebase.auth().currentUser;
-        let userID = user.uid;
-        
+        if (this.refs.note.value != '') {
+            let database = firebase.database();
+            let note = this.refs.note.value;
+            let user = firebase.auth().currentUser;
+            let userID = user.uid;
+            let oldNotes = this.state.notesArray;
 
-        database.ref('users/' + userID + '/notes' ).push().set({
-            note: note
-        })
+            database.ref('users/' + userID + '/notes').push().set({
+                note: note
+            })
+
+            oldNotes.push(note);
+            
+            this.setState({
+                notesArray: oldNotes
+            })
+
+        }
     }
 
-    componentWillUpdate(){
+    componentWillUpdate() {
         let database = firebase.database();
         let user;
         let userID;
@@ -47,60 +56,65 @@ export class TimelineComponent extends Component {
         let refAbout = database.ref("users/" + userID + "/about");
         let refMarks = database.ref("users/" + userID + "/lastmarks");
 
-        refAbout.on('value', function(snapshot){
+        refAbout.on('value', function (snapshot) {
             document.querySelector(".Descriere").innerHTML = snapshot.val().about;
         })
 
-        refMarks.on('value', function(snapshot){
+        refMarks.on('value', function (snapshot) {
             document.querySelector(".nota1").innerHTML = snapshot.val().nota4;
             document.querySelector(".nota2").innerHTML = snapshot.val().nota3;
             document.querySelector(".nota3").innerHTML = snapshot.val().nota2;
             document.querySelector(".nota4").innerHTML = snapshot.val().nota1;
         })
 
-        database.ref('users/'+userID+'/notes').on('value', snap => {
+        database.ref('users/' + userID + '/notes').on('value', snap => {
             variable = snap.val();
 
-            Object.keys(variable).map(key =>{
-                array.push(variable[key]);
+            var keys = Object.keys(variable);
+            for (var i = 0; i < keys.length; i++) {
+                var key = keys[i];
+                // Look at each fruit object!
+                array.push(variable[key].note);
+            }
+
+            this.setState({
+                notesArray:array
             })
+            //a se lucra in acest scope pt ca nu mere altundeva
+        })
 
-            
-            //a se lucra in acest scope pt ca nu mere altundeva fmm de prost si javascript <3 8====D
-        }) 
-        
     }
 
-    componentDidMount(){
-        let Track = document.querySelector( ".Track" );
-        this.maxStickys = (Track.querySelectorAll( ".notes-content-component" ).length-3)/2;
+    componentDidMount() {
+        let Track = document.querySelector(".Track");
+        this.maxStickys = (Track.querySelectorAll(".notes-content-component").length - 3) / 2;
     }
 
-    moveLeft(){
-        if(this.index >= 1)  {
-            let Track = document.querySelector( ".Track" );
+    moveLeft() {
+        if (this.index >= 1) {
+            let Track = document.querySelector(".Track");
             this.index--;
-            Track.style.transform = "translateX( -" + ( this.index * 480 ) + "px )"; 
+            Track.style.transform = "translateX( -" + (this.index * 480) + "px )";
         }
     }
 
-   moveRight(){
-    
-        if(this.index < (this.maxStickys)) { 
-            let Track = document.querySelector( ".Track" );
+    moveRight() {
+
+        if (this.index < (this.maxStickys)) {
+            let Track = document.querySelector(".Track");
             this.index++;
-            Track.style.transform = "translateX( -" + ( this.index * 480 ) + "px )";   
+            Track.style.transform = "translateX( -" + (this.index * 480) + "px )";
         }
     }
 
-    showInput(){
+    showInput() {
         document.querySelector(".homeInput").style.display = "inline-block";
         document.querySelector(".addDescriere").style.display = "none";
         document.querySelector(".Descriere").style.display = "none";
         document.querySelector(".doneDescriere").style.display = "inline-block";
     }
 
-    addAbout(){
+    addAbout() {
         let database = firebase.database();
         let user = firebase.auth().currentUser;
         let userID = user.uid;
@@ -113,44 +127,44 @@ export class TimelineComponent extends Component {
 
         database.ref("users/" + userID + "/about").set({
             about: about
-            })
+        })
 
-        this.setState({about: about});
+        this.setState({ about: about });
     }
 
     render() {
         return (
             <div className="timeline-component">
                 <div className="main_buttons">
-                    <div className="topHeaders1"> 
-                        <p className="Headers">About</p> 
+                    <div className="topHeaders1">
+                        <p className="Headers">About</p>
                         <button className="addDescriere" onClick={this.showInput}>&#9998;</button>
                         <button className="doneDescriere" onClick={this.addAbout}>&#x2714;</button>
                     </div>
-                    <div className="topHeaders2"> 
+                    <div className="topHeaders2">
                         <p className="Headers">Friends</p>
-                        </div>
-                    <div className="topHeaders3"> 
+                    </div>
+                    <div className="topHeaders3">
                         <p className="Headers">Carnet</p> </div>
                 </div>
 
                 <div className="Preview">
                     <div className="About_preview">
                         <p className="Descriere">{this.state.about}</p>
-                        <input className = "homeInput" ref={(e) => this.refs.about = e} type="text" placeholder="Something about you..."></input>
+                        <input className="homeInput" ref={(e) => this.refs.about = e} type="text" placeholder="Something about you..."></input>
                     </div>
                     <div className="Friends_preview">
                         <div className="images">
-                            <img src="https://api.adorable.io/avatars/2" alt=""/>
-                            <img src="https://api.adorable.io/avatars/3" alt=""/>
-                            <img src="https://api.adorable.io/avatars/4" alt=""/>
-                            <img src="https://api.adorable.io/avatars/5" alt=""/>
+                            <img src="https://api.adorable.io/avatars/2" alt="" />
+                            <img src="https://api.adorable.io/avatars/3" alt="" />
+                            <img src="https://api.adorable.io/avatars/4" alt="" />
+                            <img src="https://api.adorable.io/avatars/5" alt="" />
                         </div>
                         <div className="images">
-                            <img src="https://api.adorable.io/avatars/1" alt=""/>
-                            <img src="https://api.adorable.io/avatars/4" alt=""/>
-                            <img src="https://api.adorable.io/avatars/5" alt=""/>
-                            <img src="https://api.adorable.io/avatars/6" alt=""/>
+                            <img src="https://api.adorable.io/avatars/1" alt="" />
+                            <img src="https://api.adorable.io/avatars/4" alt="" />
+                            <img src="https://api.adorable.io/avatars/5" alt="" />
+                            <img src="https://api.adorable.io/avatars/6" alt="" />
                         </div>
                     </div>
                     <div className="Carnet_preview">
@@ -168,22 +182,24 @@ export class TimelineComponent extends Component {
                 </div>
                 <div className="Notes">
                     <button onClick={this.adaugaNotita} className="AddNotes" >Add note</button>
-                    <input className="noteInput" type="text"  placeholder="Your note..." maxlength="150" ref={(el) => this.refs.note = el}></input>
+                    <input className="noteInput" type="text" placeholder="Your note..." maxlength="150" ref={(el) => this.refs.note = el}></input>
                 </div>
                 <div className="MoveSticky">
-                <button onClick={this.moveLeft} className="buttonLeft">&#10096;</button>
+                    <button onClick={this.moveLeft} className="buttonLeft">&#10096;</button>
                     <div className="StickyNotes">
                         <div className="Track">
                             {
-                                this.state.notesArray.map((content)=>{ 
-                                return ( <NotesContent noteText={content} /> 
+                                this.state.notesArray.map(content => {
+                                    return (<NotesContent noteText={content} />
+                                    )
+                                }
                                 )}
-                            )}
                         </div>
                     </div>
-                <button onClick={this.moveRight} className="buttonRight">&#10097;</button>
+                    <button onClick={this.moveRight} className="buttonRight">&#10097;</button>
                 </div>
             </div>
-        )}
+        )
+    }
 
 }
