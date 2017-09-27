64d829d9c2e61a204bf2b07ede65bd34903fbb6c
Imbuzan Radu <imbuzanradu@yahoo.com>
2017-09-27 16:29:56 +0300

    fixes (for presentation)

--- src/pages/home/home.page.js
+++ src/pages/home/home.page.js
@@ -7,18 +7,54 @@ import { ProfileComponent } from '../../components/profile/profile.component';
 import { TimelineComponent } from '../../components/timeline/timeline.component';
 import { ChatSidebar } from '../../components/chat-sidebar/chat-sidebar.component';
 import { FriendsComponent } from '../../components/friends/friends.component';
+import { ArchiveComponent } from '../../components/archive/archive.component';
 
 
 export class HomePage extends Component {
+    constructor(){
+        super()
+        this.state={
+            timeline:true,
+            archive:false,
+            friends:false
+        }
+
+        this.showSelected = this.showSelected.bind(this);
+    }
+
+    showSelected(s){
+        if(s == 'Archive'){
+            this.setState({
+                timeline:false,
+                archive:true,
+                friends:false
+            })
+        }else
+        if(s == 'Timeline'){
+            this.setState({
+                timeline:true,
+                archive:false,
+                friends:false
+            })
+        }
+        else  if(s == 'Friends'){
+            this.setState({
+                timeline:false,
+                archive:false,
+                friends:true
+            })
+        }
+    }
+
     render() {
-        console.log('HomePage', this.props);
         return (
             <div className="container home-page">
                 <Header />
                 <div className="content">
-                    <ProfileComponent user={this.props.user} />
-
-                    <FriendsComponent />
+                    <ProfileComponent user={this.props.user} showSelected={this.showSelected}/>
+                    {this.state.timeline ? (<TimelineComponent />): null}
+                    {this.state.archive ? (<ArchiveComponent />): null}
+                    {this.state.friends ? (<FriendsComponent />): null}
                     <ChatSidebar />
                 </div>
             </div>
